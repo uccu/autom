f5ec52320a4565a500096e9cd0da379a53f35459
uccu <mbmbn123@hotmail.com>
2022-03-22 12:05:29 +0800

    - log

--- api/hook.go
+++ api/hook.go
@@ -3,9 +3,6 @@ package api
 import (
 	"autom/middleware"
 	"autom/service/hook"
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
 
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
@@ -20,17 +17,6 @@ func HookRegister(r *gin.RouterGroup) {
 }
 
 func (*HookController) hook(c *gin.Context) {
-
-	byt, _ := ioutil.ReadAll(c.Request.Body)
-
-	c.String(200, string(byt))
-
-	fmt.Println(string(byt))
-	fmt.Println()
-	b, _ := json.Marshal(c.Request.Header)
-	fmt.Println(string(b))
-
-	return
 
 	client := hook.NewHookClient(c)
 	if client == nil {
